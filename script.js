c1cfea6e9ed492568ecd037c1953831cee1b1b7b
unknown <parrenoemmanuel756@gmail.com>
2022-11-12 06:13:01 +0800

    Add grid

---
+++ script.js
@@ -0,0 +1,15 @@
+const grid = document.querySelectorAll('.container');
+grid.forEach((content) => {
+    for (let i =1;i<=16;i++){
+        const lol = document.createElement('div');
+        lol.classList.toggle('flex');
+        for (let j =1;j<=16;j++){
+            const lol2 = document.createElement('div');
+            lol2.classList.toggle('grid');
+            lol.appendChild(lol2);
+            content.appendChild(lol);
+        }
+        }
+    
+    })
+
