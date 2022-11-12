ff580bda0c3016fc2bac8503318766deb193b654
unknown <parrenoemmanuel756@gmail.com>
2022-11-12 10:21:32 +0800

    Add clear and new grid buttons

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-const grid = document.querySelectorAll('.container');
-grid.forEach((content) => {
-    for (let i =1;i<=16;i++){
-        const lol = document.createElement('div');
-        lol.classList.toggle('flex');
-        for (let j =1;j<=16;j++){
-            const lol2 = document.createElement('div');
-            lol2.classList.toggle('grid');
-            lol.appendChild(lol2);
-            content.appendChild(lol);
-        }
-        }
-    
-    })
+const gridSize = document.querySelector('.gridSize');
+let x = 0;
+let grid = 0;
+gridSize.addEventListener('click', ()=>{
+    if (x==0){    
+        x = prompt('grid size');
+    } else {
+        location.reload();
+    }
 
-const hover = document.querySelectorAll('.grid')
-hover.forEach((element) => {
-    element.addEventListener('mouseenter', ()=>{
-        element.classList.toggle('color')
-    })
+//main grid
+    const grid = document.querySelectorAll('.container');
+    grid.forEach((content) => {
+        if (x > 32) {
+            alert('too many lines');
+            location.reload();
+        }
+        for (let i =1;i<=x;i++){
+            const lol = document.createElement('div');
+            lol.classList.toggle('flex');
+            for (let j =1;j<=x;j++){
+                const lol2 = document.createElement('div');
+                lol2.classList.toggle('grid');
+                lol.appendChild(lol2);
+                content.appendChild(lol);
+            }}
+
+        // hover and clear
+
+            const hover = document.querySelectorAll('.grid')
+            const clear = document.querySelector('.clear');
+            hover.forEach((element) => {
+                element.addEventListener('mouseenter', ()=>{
+                    element.classList.add('color');
+                clear.addEventListener('click', () => {
+                    element.classList.remove('color')
+                    })
+                })
+            })
+            
+            
         
-    
-})
+        })
+    })
